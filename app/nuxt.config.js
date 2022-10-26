40c38d9d3a2630c7d39dcf9094c416c8b4663069
lebrancconvas <lebrancconvas@gmail.com>
2022-10-26 14:16:54 +0700

    🔧 feat(config): change web title

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - app',
-    title: 'app',
+    titleTemplate: 'Nuxt Todo',
+    title: 'Nuxt Todo',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
